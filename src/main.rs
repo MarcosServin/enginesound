820a44f3dad969e3ef9634a27caad96a3c02480f
MS:. <50627502+MarcosServin@users.noreply.github.com>
2026-05-27 00:32:21 -0300

    Set font sizes for UI themes

--- src/main.rs
+++ src/main.rs
@@ -232,6 +232,10 @@ fn main() {
                 
                 ui.theme.font_id = Some(font_id);
 
+                ui.theme.font_size_large = 24;
+                ui.theme.font_size_medium = 18;
+                ui.theme.font_size_small = 14;
+
                 let mut gui_state = GUIState::new(gui_fft_receiver);
 
                 let mut renderer = conrod_glium::Renderer::new(display.get()).unwrap();
